f6a64066b1dfda3f808acdbd6f8ee446c896c42d
listenfree <chowchi@gmail.com>
2022-08-11 09:50:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 # install dependencies
 apk add --no-cache gcc make musl-dev python3 py3-pip python3-dev
-
+pip3 install wheel
 
 # build
 pip wheel --no-deps --requirement requirements.txt
