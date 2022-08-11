505baa2b8c19a580786363090623c949ed5d1fc1
listenfree <chowchi@gmail.com>
2022-08-11 09:39:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache gcc make musl-dev
+apk add --no-cache gcc make musl-dev python3 py3-pip
 
 # build
 pip wheel --no-deps --requirement requirements.txt
